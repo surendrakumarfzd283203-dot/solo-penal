2dc7646ab67a058e498feb1aac424499927030e6
Your Name <your_email@example.com>
2026-06-30 23:56:08 +0530

    downgrade to express 4 for stability

--- web-panel/server.js
+++ web-panel/server.js
@@ -87,7 +87,7 @@ app.post('/api/login', async (req, res) => {
     res.json({ success: true, user });
 });
 
-app.get('(.*)', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
